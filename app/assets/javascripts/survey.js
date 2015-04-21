ae8c90495c7158da2aa1a64f774223e093591d6b
Marcus Schorow <mschorow@college.harvard.edu>
2015-04-20 22:46:11 -0400

    some questions added

--- app/assets/javascripts/survey.js
+++ app/assets/javascripts/survey.js
@@ -7,32 +7,206 @@ app.controller('SurveyCtrl', ['$scope', '$timeout', '$http', function($scope, $t
     // Questions should have the format
     // {
     //   question: String statement or question to be asked,
-    //   type: One of ['multiple-choice', 'agree', 'short-answer'],
+    //   type: One of ['multiple-choice', 'short-answer'],
     //   answers: List of answers in plain text,
     //   shuffle: true/false
     // }
     $scope.questions = [
+        // WELFARE
         {
-            question: 'How do you view US welfare spending?',
+          question: "These first questions will ask your opinion about the welfare system in the US.",
+          type: "info"
+        },
+        {
+            question: 'How do you view the current level of welfare spending by the US government?',
             type: 'multiple-choice',
             answers: [
-                'Too much.',
-                'Just right.',
-                'Too little.',
+                'Too much',
+                'Just right',
+                'Too little',
+                'Undecided'
             ],
             shuffle: false
         },
         {
-            question: 'How much would you agree with the following statement: Many people who receive welfare do little to improve their own condition.',
+            question: 'How much would you agree with the following statement: "Many people who receive welfare do little to improve their own condition"',
             type: 'multiple-choice',
             answers: [
                 'Strongly Disagree',
-                'Disagree',
+                'Slightly Disagree',
                 'Neutral',
-                'Agree',
+                'Slightly Agree',
                 'Strongly Agree'
             ],
             shuffle: false
+        },
+        {
+            question: 'What is your opinion about the current way that welfare is handled in the US?',
+            type: 'multiple-choice',
+            answers: [
+                'Very Unfavorable',
+                'Slightly Unfavorable',
+                'Neutral',
+                'Slightly Favorable',
+                'Very Favorable',
+                'Undecided'
+            ],
+            shuffle: false
+        },
+        // SECURITY
+        {
+            question: "These next questions will ask questions about the security of the US.",
+            type: "info"
+        },
+        {
+            question: 'How much danger do you believe Iran\'s nuclear weapon program poses to the US?',
+            type: 'multiple-choice',
+            answers: [
+                'No Danger',
+                'Slight Danger',
+                'Moderate Danger',
+                'Extreme Danger',
+                'Undecided'
+            ],
+            shuffle: false
+        },
+        {
+            question: 'Would you favor the US taking military action against Iran if this were the only way to stop them from developing nuclear weapons?',
+            type: 'multiple-choice',
+            answers: [
+                'Strongly Oppose',
+                'Slightly Oppose',
+                'Neutral',
+                'Slightly Favor',
+                'Strongly Favor',
+                'Undecided'
+            ],
+            shuffle: false
+        },
+        // NSA
+        {
+            question: "These next questions will ask about the National Security Administration (NSA). If you are not familiar with the" +
+            "programs mentioned, feel free to select 'Undecided' from the answer choices.",
+            type: "info"
+        },
+        {
+            question: 'Do you believe the NSA wrongfully violated civil liberties with its PRISM program?',
+            type: 'multiple-choice',
+            answers: [
+                'Yes',
+                'No',
+                'No Opinion / Undecided'
+            ],
+            shuffle: false
+        },
+        {
+            question: 'How personally threatened do you feel by the NSA\'s actions?',
+            type: 'multiple-choice',
+            answers: [
+                'Not At All',
+                'Slightly Threatened',
+                'Moderately Threatened',
+                'Extremely Threatened',
+                'Undecided'
+            ],
+            shuffle: false
+        },
+        {
+          type: "info",
+          question: "These next questions will ask you to consider two hypothetical presidential candidates and select which one you would be more likely to vote for."
+        },
+        {
+            
+        }
+        // TODO: CANDIDATES STUFF IF WE WANTS
+
+        {
+            question: "These next questions will ask for your opinion of how the US government is performing in their duties.",
+            type: "info"
+        },
+        {
+            question: 'What do you think of the job that Obama is currently doing as president?',
+            type: 'multiple-choice',
+            answers: [
+                'Extremely Unfavorable',
+                'Slightly Unfavorable',
+                'Neutral',
+                'Slightly Favorable',
+                'Extremely Favorable',
+                'Undecided'
+            ],
+            shuffle: false
+        },
+        {
+            question: 'What do you think of the job that Congress is currently doing?',
+            type: 'multiple-choice',
+            answers: [
+                'Extremely Unfavorable',
+                'Slightly Unfavorable',
+                'Neutral',
+                'Slightly Favorable',
+                'Extremely Favorable',
+                'Undecided'
+            ],
+            shuffle: false
+        },
+
+        // Demographics
+        {
+            question: "This concludes the main part of our survey. These final questions will ask some demographic data about your political background. To be entered in the drawing for the Amazon gift card, you need to completely all of these questions.",
+            type: "info",
+            noDelay: true
+        },
+        {
+            question: "How would you classify your political ideologies?",
+            type: 'multiple-choice',
+            answers: [
+                'Extremely Liberal',
+                'Slightly Liberal',
+                'Neutral',
+                'Slightly Conservative',
+                'Extremely Conservative',
+                'Undecided / Apolitcal'
+            ],
+            noDelay: true
+        },
+        {
+            question: "How passionate about the issues mentioned in this survey would you say you are?",
+            type: 'multiple-choice',
+            answers: [
+                'Not At All',
+                'Slightly Passionate',
+                'Moderately Passionate',
+                'Extremely Passionate'
+            ],
+            noDelay: true
+        },
+        {
+            question: "How likely are you to vote in a 2016 Presidential Primary race?",
+            type: 'multiple-choice',
+            answers: [
+                'Extremely Unlikely',
+                'Slightly Unlikely',
+                'Neither Likely Nor Unlikely',
+                'Slightly Likely',
+                'Extremely Likely'
+            ],
+            noDelay: true
+        },
+        {
+            question: "The final questions ask about the methodologies used in our survey.",
+            type: 'info',
+            noDelay: true
+        },
+        {
+            question: "Do you have any comments about the methoodology of this survey, or did you have any issues while taking the survey?",
+            type: 'short-answer',
+            noDelay: true
+        },
+        {
+            question: "For the drawing to win an Amazon gift card, what do you believe would be the fair amount of money to put on the card to give the winner?",
+            type: 'short-answer',
+            noDelay: true
         }
     ];
 
@@ -201,7 +375,7 @@ app.controller('SurveyCtrl', ['$scope', '$timeout', '$http', function($scope, $t
             } else if(type === 'short-answer') {
                 $scope.questionMode = $scope.SHORT_ANSWER;
             } else {
-                alert('MARCUS YOU FUCKED UP!');
+                alert('MARCUS YOU HAVE MADE A SLIGHT ERROR - ITS OKAY I FORGIVE YOU!');
             }
 
             $scope.curAnswer = null;
